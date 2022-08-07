a02afdaf589875f769ef5571cd9efc5466014e22
hagukin <hagukin@naver.com>
2022-08-07 23:56:15 +0900

    Update 3687.cpp
    반례가 N=17일때 나와서 오래 걸린 문제

--- boj/3687.cpp
+++ boj/3687.cpp
@@ -103,50 +103,44 @@ i 오답 정답 순서 (i는 최솟값 dp 인덱스)
 // 답안
 #include <iostream>
 #include <climits>
- 
 using namespace std;
- 
-int n, testcase;
+typedef long long ll;
+constexpr ll INF = 888888888888888;
+int n, TC;
 int num[9] = {0, 0, 1, 7, 4, 2, 0, 8, 10};
-long long minDP[101] = {0, };
- 
+ll dp[101] = {0, };
+
 void minCal()
 {
-    for(int i=1; i<=9; i++){
-        minDP[i] = num[i];
+    for(int i=1; i<=9; i++)
+    {
+        dp[i] = num[i];
     }
-    minDP[6] = 6; // 처음 시작은 0으로 할 수 없으니, 6으로 시작
-    // 또한, 9부터 시작하는 이유는, 8부터 하면 10이 아닌, 08(j=7일 경우) 값을 최소로 인식한다.
-    for(int i=9; i<=100; i++){
-        minDP[i] = 888888888888888;
-        for(int j=2; j<8; j++){
-            minDP[i] = min(minDP[i], minDP[i-j]*10+num[j]);
+    dp[6] = 6; // 0으로 시작하면 안됨
+    for(int i=9; i<=100; i++)
+    {
+        dp[i] = INF;
+        for(int j=2; j<8; j++)
+        {
+            dp[i] = min(dp[i], dp[i-j]*10+num[j]); // num과 dp 분리 이유: 0과 6
         }
     }
 }
  
 int main()
 {
-    cin>>testcase;
+    cin >> TC;
     minCal(); 
-    while(testcase--){
-        cin>>n;
-        // 최소
-        cout<<minDP[n]<<" ";
- 
-        // 최대(홀수, 짝수 나누어서)
-        while(n){
-            if(n % 2 != 0){
-                cout << 7;
-                n -= 3;
-            }
-            else{
-                cout << 1;
-                n -= 2;
-            }
+    while(TC--)
+    {
+        cin >> n;
+        cout << dp[n] << " ";
+        while (n)
+        {
+            if(n % 2 != 0) {cout << 7;n -= 3;}
+            else {cout << 1; n -= 2;}
         }
         cout << "\n";
     }
- 
     return 0;
 }
