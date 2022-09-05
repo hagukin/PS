1f242198a67ea6d2b9bde546145260ee5784960c
hagukin <hagukin@naver.com>
2022-09-05 20:28:29 +0900

    Update 17780.cpp

--- boj/unsolved/17780.cpp
+++ boj/unsolved/17780.cpp
@@ -1,65 +1,129 @@
 #include <iostream>
 #include <list>
+#include <algorithm>
 using namespace std;
 constexpr int MAXN = 14;
-
-// TODO: 시간없어서 일단 스킵
-// 작성할거 많음
-
 struct Piece
 {
-private:
     int _y, _x;
-    int _dir; // rlud 1234
-public:
-    list<int>& pStack; // 이 말이 속해있는 stack. l->r: 맨 위에서 맨 아래로
-    void getY(int idx)
-    {
-        if (canMove(idx)) return pieces[idx]._y;
-        else return getY(pieces[idx].pStack.back());
-    }
-    
-    void getX(int idx)
-    {
-        if (canMove(idx)) return pieces[idx]._x;
-        else return getX(pieces[idx].pStack.back());
-    }
-    
-    void setY(int idx, int val)
-    {
-        pieces[idx].y = val;
-        if (canMove(idx))
-    }
-    
-    void setX(int idx, int val)
-    {
-        
-    }
+    int dydx; // rlud 1234
+    list<int> pStack; // 이 말이 속해있는 stack. l->r: 맨 위에서 맨 아래로
 };
 
 int N, K, ret=-1;
 int gridColor[14][14] = {0,};
-int gridPieces[14][14]; // 해당 칸의 가장 아래에 있는 말의 번호. 없으면 0
-int pieces[11];
-pair<int,int> nToDir[4] = {{0,0},{1,0},{-1,0},{0,-1},{0,1}}; // rlud 1234
+int gridPieces[14][14] = {0,}; // 해당 칸의 가장 아래에 있는 말의 번호. 없으면 0
+Piece pieces[11];
+pair<int,int> nToDir[5] = {{0,0},{1,0},{-1,0},{0,-1},{0,1}}; // rlud 1234
+int reverseDir[5] = {0,2,1,4,3};
 
+bool canMove(int idx)
+{
+    // idx번 말이 가장 아래에 있는가?
+    return idx == pieces[idx].pStack.back();
+}
+
+int getY(int idx)
+{
+    if (canMove(idx)) return pieces[idx]._y;
+    else return getY(pieces[idx].pStack.back());
+}
+
+int getX(int idx)
+{
+    if (canMove(idx)) return pieces[idx]._x;
+    else return getX(pieces[idx].pStack.back());
+}
+
+void setY(int idx, int val)
+{
+    for (int nidx : pieces[idx].pStack)
+    {
+        pieces[nidx]._y = val;
+    }
+}
+
+void setX(int idx, int val)
+{
+    for (int nidx : pieces[idx].pStack)
+    {
+        pieces[nidx]._x = val;
+    }
+}
 
 int getTileColor(int cy, int cx, int dy, int dx)
 {
     return gridColor[cy+dy][cx+dx];
 }
 
-bool canMove(int idx)
+void moveAndStack(int idx, int cy, int cx, int dy, int dx)
 {
-    // idx번 말이 가장 아래에 있는가?
-    return idx == piece[idx].pStack.back();
+    int ny = cy+dy; 
+    int nx = cx+dx;
+    int tp = gridPieces[ny][nx];
+    if (tp)
+    {
+        // 리스트 합치기
+        pieces[tp].pStack.splice(pieces[tp].pStack.end(), pieces[idx].pStack);
+    }
+    // 기존 위치에서 정보 삭제
+    gridPieces[pieces[idx]._y][pieces[idx]._x] = 0;
+    // 동기화
+    for (int nidx : pieces[idx].pStack)
+    {
+        pieces[nidx]._y = ny;
+        pieces[nidx]._x = nx;
+        pieces[nidx].pStack = pieces[tp].pStack;
+    }
+    // 새 위치 정보 입력
+    gridPieces[ny][nx] = pieces[idx].pStack.back();
+    return;
+}
+
+void reverseCurrStack(int idx)
+{
+    // idx번 말이 속해있는 스택에서 idx를 기준으로 idx 및 그 위에 있는 모든 말들을 뒤집는다.
+    list<int>& st = pieces[idx].pStack;
+    auto idxPos = st.begin();
+    for (int c : st)
+    {
+        if (c == idx) break;
+        idxPos++;
+    }
+    reverse(idxPos, st.end());
+    return;
 }
 
 bool processMove(int idx)
 {
     // return: 길이 4 이상의 stack이 만들어졌는가?
     if (!canMove(idx)) return false;
-    
+    pair<int,int> nDir = nToDir[pieces[idx].dydx];
+    int tColor = getTileColor(getY(idx), getX(idx), nDir.first, nDir.second);
+    switch(tColor)
+    {
+        case 0:
+            moveAndStack(idx, getY(idx), getX(idx), nDir.first, nDir.second);
+            break;
+        case 1:
+            moveAndStack(idx, getY(idx), getX(idx), nDir.first, nDir.second);
+            reverseCurrStack(idx);
+            break;
+        case 2:
+            pieces[idx].dydx = reverseDir[pieces[idx].dydx];
+            nDir = nToDir[pieces[idx].dydx];
+            if (getTileColor(getY(idx), getX(idx), nDir.first, nDir.second) == 2) break;
+            else moveAndStack(idx, getY(idx), getX(idx), nDir.first, nDir.second);
+            break;
+        default:
+            break;
+    }
+    cout << idx << getX(idx) << getY(idx) << endl;
+    for (int i : pieces[idx].pStack)
+    {
+        cout << i << "S" << endl;
+    }
+    if (pieces[idx].pStack.size() >= 4) return true;
     return false;
 }
 
@@ -100,9 +164,11 @@ int main()
     for (int i=0;i<K;i++)
     {
         cin >> ty >> tx >> tDir;
-        Piece np = {ty, tx, tDir}; // 두 말이 겹친 상태는 입력으로 주어지지 않는다.
-        piece[i] = np;
-        gridPieces[ty][tx].push_back(i);
+        list<int> nl;
+        nl.push_back(i);
+        Piece np = {ty, tx, tDir, nl}; // 두 말이 겹친 상태는 입력으로 주어지지 않는다.
+        pieces[i] = np;
+        gridPieces[ty][tx] = i;
     }
     
     // Solve
@@ -111,7 +177,6 @@ int main()
         if (processTurn())
         {
             ret = turn;
-            gameCouldEnd = true;
             break;
         }
     }
