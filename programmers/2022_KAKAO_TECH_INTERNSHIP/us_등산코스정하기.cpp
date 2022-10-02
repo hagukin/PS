937ba96998304604757252f1f507ca7a4d664158
hagukin <hagukin@naver.com>
2022-10-02 23:50:28 +0900

    Update us_등산코스정하기.cpp
    kim519620.tistory.com/entry/등산코스-정하기-feat-프로그래머스-118669번 참고
    해당 풀이에서는 adjList 구현에 set을 썼는데 그게 더 안전하긴 하지만 꼭 그럴 필요는 없어서 그냥 vector를 사용함

--- programmers/2022_KAKAO_TECH_INTERNSHIP/us_등산코스정하기.cpp
+++ programmers/2022_KAKAO_TECH_INTERNSHIP/us_등산코스정하기.cpp
@@ -1,181 +1,72 @@
+#include <string>
 #include <queue>
 #include <vector>
-#include <iostream>
-#include <memory.h>
 
 using namespace std;
 
-struct Path
-{
-    int start, goal, dist;
-};
-
-struct cmp
-{
-    bool operator()(const Path& p1, const Path& p2)
-    {
-        if (p1.dist == p2.dist) return p1.goal < p2.goal;
-        return p1.dist > p2.dist;
-    }
-};
+int intensity[50001];
+char nodeInfo[50001] = {0,};
+vector<pair<int,int>> adj[50001]; // 노드i의 인접노드와 그 거리를 저장
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
-    vector<int> answer;
+    constexpr int INF = 987654321;
     
-    /*
-    결과는 무조건 대칭이 나옴 (예시에서 비대칭이긴한데 이건 대칭으로도 정답구하기가 가능)
-    고로 우리는 intensity가 최소인 출발-정상까지의 경로만 구하면 됨
+    for (int i=1;i<=n;++i) 
+        intensity[i] = INF;
+    for (auto& g : gates)
+        nodeInfo[g] = 'G';
+    for (auto& s : summits)
+        nodeInfo[s] = 'S';
     
-    <시간초과: Brute force 50000^4>
-    for 모든 정상
-    for 모든 출입구
-    DFS로 출입구-정상 찾고 intensity 값 구하기
-    
-    <DP로 최적화>
-    priority_queue (heap) 사용
-    for 모든 출발지
-    해당 출발지의 등산로 정보 추가, 거리짧은 순으로 정렬, 거리가 같으면 번호 작은순으로 정렬
-    이때 등산로는 양방향인데, 어느 방향으로 나아갈지 어떻게 아느냐?
-    -> 방문한 노드들은 visited 처리해주고, visited에서 nonvisited로 이동하는 경로로 이동해 다음 노드를 정하면됨.
-    
-    가장 짧은 길부터 선택, 이동후 이어진 노드를 추가 (=즉 이어진 노드의 등산로들도 다 힙에 추가)
-    int intensities[n+1]
-    intensities[i]번에 해당 추가된 노드까지 가는 intensity를 갱신(max(현재노드, 도착노드))
-    */
-    
-    /*
-    구현:
-    1) 노드i가 가진 등산로 정보들을 가져올 수 있어야 함.
-    2) 노드i ~ j의 dist를 구할 수 있어야 함.
-    
-    -> 입력된 paths를 변형해서 다음과 같은 형태로 만들기
-    newPaths[노드번호][<=n]
-    newPaths[i]에는 i의 등산로 정보들이 들어있음.
-    ex. newPaths[1] = {{1,2,4},{1,5,12}, {1,8,2}} 이때 start노드도 기록 필요함.
-    1~2는 4, 1~5는 12 이런 식
-    
-    이렇게 하면 모든 출발지에 대해 각 출발지의 등산로 정보를 추가하는데 O(V+E)로 할 수 있음.
-    
-    3) visited 배열 만들기
-    
-    4) 어떤 노드가 gate인지, summit인지를 O(1)에 알 수 있어야 함.
-    newPaths;
-    bool isGate[50001]; 
-    bool isSummit[50001];
-    vector<int> answerCandidates[50001]; // 각 s별 ret값
-    
-    // 등산로 입력
-    for 출발노드 s
+    // 필요없는 등산로 제거
+    for (auto& p : paths)
     {
-        bool visited[50001];
-        int intensity[50001];
-        priority_queue pq;
-        vector<int> ret; // 이 s의 경우의 ret값
+        // 시작이 게이트 또는 도착이 산정상 (이 부분을 고려 안해서 틀림)
+        if (nodeInfo[p[0]] == 'G' || nodeInfo[p[1]] == 'S')
+            adj[p[0]].push_back(make_pair(p[1],p[2]));
+        // 도착이 게이트 또는 시작이 산정상
+        else if (nodeInfo[p[0]] == 'S' || nodeInfo[p[1]] == 'G')
+            adj[p[1]].push_back(make_pair(p[0],p[2]));
+        else // 그 왜의 경우는 양방향 모두 adj에 추가해준다
+            adj[p[0]].push_back(make_pair(p[1],p[2])), adj[p[1]].push_back(make_pair(p[0],p[2]));
+    }
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; // pair는 거리, 도착정점 순
+    for (auto& g : gates)
+    {
+        pq.push(make_pair(0, g)); // 중요: 모든 시작지점을 그냥 처음부터 pq에 다 넣어버린다
+        // 처음 풀 때 이렇게 안하고 각 gates 별로 개별적으로 pq를 만들어 탐색하다 보니까 시간초과가 났음 - worst case O(V*V)
+        // 이렇게 시작지점별 구분 없이 그냥 모든 케이스를 한번에 탐색하면 훨씬 빠름 - O(E) (200000)
+        intensity[g] = 0;
+    }
+    while (!pq.empty())
+    {
+        pair<int,int> cRoad = pq.top();
+        pq.pop();
+        int dist = cRoad.first;
+        int cNode = cRoad.second;
+        if (intensity[cNode] < dist) continue; // 중요: 다른 시작지점에서 출발한 경로중 이 노드에 더 낮은 intensity로 방문하는 경우가 있을 경우 현재의 시작지점에서는 해당 노드를 더 탐색할 필요가 없다.
         
-        visited[s] = true;
-        for s의 등산로 p:
-            if (visited[p.goal]) continue;
-            pq.push(p)
-        
-        while(!pq.empty())
+        // 인접 노드들 방문
+        for (auto& nRoad : adj[cNode])
         {
-            currP = pq.pop();
-            visited[currP.goal] = true;
-            intensity[currP.goal] = max(currP.dist, intensity[currP.start]);
-            if currP.goal이 산봉우리면:
-                if ret[1] > intensity[currP.goal]이면:
-                    ret 새로 덮어쓰기
-
-            for currP.goal의 등산로 pp:
-                if (visited[pp.goal]) continue;
-                pq.push(pp)
-        }
-        
-        // 이 시점에는 이미 ret 구해짐
-        answerCandidates[s] = ret;
-    }
-    */
-    
-    // 1
-    vector<vector<Path>> newPaths;
-    for (int i=0;i<=n;i++) 
-    {
-        vector<Path> nv;
-        newPaths.push_back(nv);
-    }
-    for (int i=0;i<paths.size();i++)
-    {
-        Path np = {paths[i][0], paths[i][1], paths[i][2]};
-        newPaths[paths[i][0]].push_back(np);
-        np = {paths[i][1], paths[i][0], paths[i][2]};
-        newPaths[paths[i][1]].push_back(np);
-    }
-    
-    // 2
-    bool isSummit[50001];
-    for (int i=0;i<summits.size();i++)
-    {
-        isSummit[summits[i]] = true;
-    }
-    
-    // 3
-    vector<int> answerCandidates[50001];
-    
-    // solve
-    for (int i=0;i<gates.size();i++)
-    {
-        bool visited[50001] = {0,};
-        int intensity[50001] = {0,};
-        priority_queue<Path, vector<Path>, cmp> pq;
-        vector<int> cRet;
-        cRet.push_back(987654321);
-        cRet.push_back(987654321); // {INF, INF}
-        
-        const int& s = gates[i];
-        visited[s] = true;
-        for (int j=0;j<newPaths[s].size();j++)
-        {
-            Path cPath = newPaths[s][j];
-            if (visited[cPath.goal]) continue;
-            pq.push(cPath);
-        }
-        
-        // main logic
-        while (!pq.empty())
-        {
-            Path currP = pq.top();
-            pq.pop();
-            if (visited[currP.goal]) continue;
-            cout << currP.start << currP.goal << currP.dist << endl;
-            visited[currP.goal] = true;
-            
-            intensity[currP.goal] = max(currP.dist, intensity[currP.start]);
-
-            for (int j=0;j<newPaths[currP.goal].size();j++)
+            int nextNode = nRoad.first;
+            int nodeDist = nRoad.second;
+            if (intensity[nextNode] > max(nodeDist, dist))
             {
-                Path cPath = newPaths[currP.goal][j];
-                if (visited[cPath.goal]) continue; // 없애도되나?
-                pq.push(cPath);
-            }
-            
-            if (isSummit[currP.goal])
-            {
-                if (cRet[1] > intensity[currP.goal] && currP.start != s)
-                {
-                    cRet[0] = currP.goal;
-                    cRet[1] = intensity[currP.goal];
-                }
+                intensity[nextNode] = max(nodeDist, dist);
+                pq.push(make_pair(intensity[nextNode], nextNode));
             }
         }
-        answerCandidates[s] = cRet;
-        
-        
-        cout << s << " s " << endl;
-        for (int i=1;i<7;i++)
-        {
-            cout << intensity[i] << " ";
-        }
-        cout << endl;
+    }
+    
+    vector<int> answer{INF, INF};
+    for (auto& s : summits)
+    {
+        if (intensity[s] < answer[1])
+            answer[0] = s, answer[1] = intensity[s];
+        else if (intensity[s] == answer[1])
+            answer[0] = min(s, answer[0]);
     }
     
     return answer;
