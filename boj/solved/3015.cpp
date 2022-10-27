dcf49ac1a75c6041d399530aa417b1a925c45881
hagukin <hagukin@naver.com>
2022-10-27 23:30:16 +0900

    Update 3015.cpp

--- boj/solved/3015.cpp
+++ boj/solved/3015.cpp
@@ -1,58 +1,56 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int N;
-vector<pair<int,int >> arr;
-int ret = 0;
+long N;
+vector<pair<long,long >> arr;
+long ret = 0;
 
 int main()
 {
     cin >> N;
-    pair<int,int> tmp;
-    for (int i=0;i<N;++i)
+    pair<long,long> tmp;
+    long i=0; long prevI=-1;
+    while (i<N)
     {
-        tmp.second = 1;
-        cin >> tmp.first;
-        
+        // 같은 인풋에 대해 한번 더 루프를 도는 경우 cin을 무시하고 계속 이전 tmp값으로 진행
+        if (i>prevI)
+        {
+            tmp.second = 1;
+            cin >> tmp.first;
+            prevI = i;
+        }
+    
         if (arr.empty()) // 빈 경우
         {
             arr.push_back(tmp);
-            continue;
+            i++; //끝냄
         }
-        else if (arr[arr.size()-1].first == tmp.first) // 크기가 같은 경우
+        else if (arr.back().first == tmp.first) // 크기가 같은 경우
         {
-            ret += arr[arr.size()-1].second;
-            cout << "같은 경우 +" << arr[arr.size()-1].second << endl;
-            if (arr.size()-1 > 0) {ret += 1; cout << "같은경우 추가로 큰거랑 마주침 +1" << endl;} // 뒤에 큰게 하나 이상이 있음이 보장되는 경우
-            arr[arr.size()-1].second++;
+            ret += arr.back().second;
+            arr.back().second++;
+            //다시 루프 진행할 필요X, 대신 뒤에 큰게 있을 경우 +1
+            if (arr.size() >= 2) ret += 1;
+            i++; //끝냄
         }
-        else // 크기가 다른 경우
+        else
         {
-            if (arr[arr.size()-1].first > tmp.first) // 큰 경우
+            if (arr.back().first > tmp.first) //큰 경우
             {
                 ret += 1;
-                cout << "큰경우 +1" << endl;
+                arr.push_back(tmp);
+                i++; //끝냄
             }
-            else // 작은 경우
+            else //작은 경우
             {
-                while(!arr.empty() && arr[arr.size()-1].first < tmp.first)
+                while(!arr.empty() && arr.back().first < tmp.first)
                 {
                     ret += arr.back().second;
-                    cout << "작은경우 +" << arr.back().second << endl;
-                    arr.pop_back();
+                    arr.pop_back(); //pop후 다시 루프 (그 뒤에 같거나 큰걸 만날 수 있으므로)
                 }
-                if (!arr.empty()) ret++; // 큰거와 만남
             }
-            arr.push_back(tmp);
         }
     }
-    
-    //test
-    for (auto& x : arr)
-    {
-        cout << x.first << x.second << endl;
-    }
-    
     cout << ret << '\n';
     
     return 0;
