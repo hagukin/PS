0d7a9e0dad9b46fe4e1dc4508104d1e06c7b5f62
Haguk Kim <hagukin@naver.com>
2023-01-21 16:09:43 +0900

    Update 1238.cpp

--- boj/solved/1238.cpp
+++ boj/solved/1238.cpp
@@ -6,8 +6,6 @@ using namespace std;
 int N, M, X;
 vector<vector<int >> originRoad;
 vector<vector<int >> reversedRoad;
-int toX[1001]; // 노드 i에서 X까지 가는 최단거리
-int fromX[1001]; // X에서 노드 i까지 오는 최단거리
 constexpr int INF = 987654321;
 
 vector<int> djikstra1(int startN, const vector<vector<int >>& road)
@@ -51,45 +49,46 @@ vector<int> djikstra1(int startN, const vector<vector<int >>& road)
 
 struct S
 {
-    int node, value=INF;
+    int node, value;
 };
 
 struct cmp
 {
     bool operator()(const S& s1, const S& s2)
     {
-        return s1.value < s2.value;
+        return s1.value > s2.value; // pq는 큰 값이 먼저 나옴
     }
 };
 
-
-// vector<int> djikstra2(int startN, const vector<vector<int >>& road)
-// {
-//     bool visited[1001] = {false,};
+vector<int> djikstra2(int startN, const vector<vector<int >>& road)
+{
+    bool visited[1001] = {false,};
     
-//     vector<int> dist(INF, 1001);
-//     dist[startN] = 0;
+    vector<int> dist(1001, INF);
+    dist[startN] = 0;
     
-//     priority_queue<S, vector<S>, cmp> pq;
-//     S curr = {startN, 0};
-//     pq.push(curr);
+    priority_queue<S, vector<S>, cmp> pq;
+    S curr = {startN, 0};
+    pq.push(curr);
     
-//     while(!pq.empty())
-//     {
-//         curr = pq.top();
-//         pq.pop();
-//         visited[curr] = true;
+    while(!pq.empty())
+    {
+        curr = pq.top();
+        pq.pop();
+        visited[curr.node] = true;
         
-//         for (int i=0;i<1001;++i)
-//         {
-//             if (road[curr.node][i] && !visited[i])
-//             {
-//                 dist[i] = min(dist[curr.node] + road[curr.node][i], dist[i]);
-//             }
-//         }
-//     }
-//     return dist;
-// }
+        for (int i=1;i<=N;++i)
+        {
+            if (road[curr.node][i]!=INF && !visited[i] && dist[curr.node] + road[curr.node][i] < dist[i])
+            {
+                dist[i] = dist[curr.node] + road[curr.node][i];
+                S newNode = {i,dist[i]};
+                pq.push(newNode);
+            }
+        }
+    }
+    return dist;
+}
 
 int main()
 {
@@ -113,8 +112,8 @@ int main()
         reversedRoad[b][a] = originRoad[a][b];
     }
     
-    vector<int> v = djikstra1(X, originRoad);
-    vector<int> v2 = djikstra1(X, reversedRoad);
+    vector<int> v = djikstra2(X, originRoad);
+    vector<int> v2 = djikstra2(X, reversedRoad);
     
     int ret = -1;
     for (int i=1;i<=N;++i)
