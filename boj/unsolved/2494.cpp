6bef2675dd5297f70992f03206912e6183d119d6
Haguk Kim <hagukin@naver.com>
2023-01-22 11:16:51 +0900

    Update 2494.cpp
    기존 시간초과 나던 풀이에 메모이제이션을 적용했다.
    문제는 메모이제이션을 적용하는 과정에서 재귀 파라미터를 변환했는데, 이렇게 변경 이후에는 경로추적이 불가능해진다.
    현재 함수 형태로 경로 전체를 다 메모이제이션 하려면 [10000][10000]이 필요해 메모리가 부족해진다.
    즉 기존 풀이: 경로 추적이 가능하지만 메모이제이션이 불가해 시간초과, 현재 풀이: 메모이제이션이 가능하지만 경로추적이 불가능
    이걸 해결하려면 경로를 메모이제이션하되, 경로 전체를 메모이제이션하지 않고도 일부만 저장해 경로를 추출하는 방법을 찾아야 할 듯 하다.
    생각보다 상당히 복잡한 문제다

--- boj/unsolved/2494.cpp
+++ boj/unsolved/2494.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
 using namespace std;
-constexpr int INF = 987654321;
-int N;
 
 int getDiff(int a, int b)
 {
@@ -12,27 +11,25 @@ int getDiff(int a, int b)
     return tmp;
 }
 
+int N;
 string A, B;
-int retCnt = INF;
-vector<int> retMoves;
+int memo[10001][10]; // memo[depth][왼쪽으로 돌린 수 % 10] = 남은 수(depth+1 이상)들을 다 맞히기 위해 추가적으로 필요한 최소 회전수
 
-int recursive(int depth, int cnt, int posMoves, vector<int>& moves)
+int recursive(int depth, int posMoves)
 {
+    // 반환: memo와 동일 
     // depth: 현재 depth번 자리까지 맞혀져 있는 상태
-    // cnt: 누적회전수
     // posMoves: 그동안 상위 숫자나사에서 좌측으로 돌린 누적횟수. 
     // moves: 현재까지의 회전 방향과 횟수 기록
     // 이만큼 먼저 돌려놓은 상태에서 시작해야 함.
     
     if (depth == N) // 기저, 다맞힘
     {
-        if (cnt < retCnt)
-        {
-            retCnt = cnt;
-            retMoves = moves; // deepcopy
-        }
-        return cnt;
+        return 0;
     }
+    
+    if (memo[depth][posMoves%10] != -1)
+        return memo[depth][posMoves%10];
     
     int cA = ((A[depth]-'0')+posMoves)%10;
     int cB = B[depth]-'0';
@@ -40,23 +37,16 @@ int recursive(int depth, int cnt, int posMoves, vector<int>& moves)
     int posM = getDiff(cA,cB); // 왼쪽 회전
     int negM = posM - 10; // 우측 회전 (음수)
     
-    //cout << "d: " << depth+1 << " cnt: " << cnt << " posMoves: " << posMoves  << " cA: " << cA << " cB: " << cB << " move: " << posM << "vs" << negM << endl;
+    int posCase = recursive(depth+1, posMoves+posM);
+    int negCase = recursive(depth+1, posMoves);
     
-    moves.push_back(posM);
-    int posCase = recursive(depth+1, cnt+posM, posMoves+posM, moves);
-    moves.pop_back();
-    
-    moves.push_back(negM);
-    int negCase = recursive(depth+1, cnt+abs(negM), posMoves, moves);
-    moves.pop_back();
-    
-    if (posCase < negCase)
+    if (posCase+posM < negCase-negM)
     {
-        return posCase;
+        return memo[depth][posMoves%10] = posCase+posM;
     }
     else
     {
-        return negCase;
+        return memo[depth][posMoves%10] = negCase-negM;
     }
 }
 
@@ -64,11 +54,8 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
+    memset(memo, -1, sizeof(memo));
     cin >> N >> A >> B;
-    vector<int> v;
-    cout << recursive(0, 0, 0, v) << '\n';
-    for (int i=0;i<N;++i)
-        cout << i+1 << " " << retMoves[i] << '\n';
+    cout << recursive(0, 0) << '\n';
     return 0;
 }
