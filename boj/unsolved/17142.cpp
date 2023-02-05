380e16da65c27df8c39b293fb0d7f95f4a2d3534
Haguk Kim <hagukin@naver.com>
2023-02-05 17:43:22 +0900

    Update 17142.cpp

--- boj/unsolved/17142.cpp
+++ boj/unsolved/17142.cpp
@@ -2,18 +2,85 @@
 #include <vector>
 using namespace std;
 typedef pair<int,int> P; // y,x
+constexpr int INF = 999999999;
 constexpr int WALL = -1;
 constexpr int BLANK = 987654321;
+constexpr int VIRUS = 0;
 
 int N, M;
-int ret = -1;
+int ret = INF;
 int initGrid[51][51];
 int virusCnt = 0; // 비활성, 활성 모두 포함한 바이러스 수 (2 갯수)
 P viruses[10]; // 시작 바이러스들에 0~9까지의 id를 매기고 각 바이러스의 좌표 저장
+int directions[4][2] = {{0,1},{1,0},{0,-1},{-1,0}}; // dydx
+
+void debug(vector<vector<int >>& cGrid)
+{
+    /*** DEBUG ***/
+    for (int i=0;i<N;++i)
+    {
+        for (int j=0;j<N;++j)
+        {
+            if (cGrid[i][j] == WALL) cout << "#  ";
+            else if (cGrid[i][j] == BLANK) cout << ".  ";
+            else if (cGrid[i][j] < 10) cout << cGrid[i][j] << "  ";
+            else cout << cGrid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "================" << endl;
+}
 
 void spread(vector<vector<int >>& cGrid, vector<P>& roots)
 {
-    return; // TODO
+    while (!roots.empty())
+    {
+        P cRoot = roots.back();
+        roots.pop_back();
+        
+        vector<P> tiles;
+        tiles.push_back(cRoot);
+        cGrid[cRoot.first][cRoot.second] = 0; // 반드시 필요한 코드, 새 루트에서 0으로 시작하기 위해 필수
+        
+        while(!tiles.empty())
+        {
+            P cTile = tiles.back();
+            tiles.pop_back();
+            int cn = cGrid[cTile.first][cTile.second];
+            
+            for (int i=0;i<4;++i)
+            {
+                int ny = cTile.first+directions[i][0];
+                int nx = cTile.second+directions[i][1];
+                
+                if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+                if (cGrid[ny][nx] == WALL) continue;
+                else if (cGrid[ny][nx] == BLANK || cGrid[ny][nx] == VIRUS) // 빈칸 혹은 선택된 바이러스
+                {
+                    cGrid[ny][nx] = cn+1;
+                    
+                    P nTile;
+                    nTile.first = ny;
+                    nTile.second = nx;
+                    tiles.push_back(nTile);
+                }
+                else // 수 겹침
+                {
+                    if (cn+1 <= cGrid[ny][nx]) // 내 수가 같거나 더 작음
+                    {
+                        cGrid[ny][nx] = cn+1; // 덮어쓴다
+                        
+                        P nTile;
+                        nTile.first = ny;
+                        nTile.second = nx;
+                        tiles.push_back(nTile);
+                    }
+                    else continue; // 내가 더 크면 현재 탐색방향으로의 탐색중단
+                }
+            }
+        }
+    }
+    return;
 }
 
 inline bool isAllFilled(vector<vector<int>> cGrid)
@@ -28,8 +95,9 @@ inline bool isAllFilled(vector<vector<int>> cGrid)
     return true;
 }
 
-void doWithComb(vector<int>& cv)
+int doWithComb(vector<int>& cv)
 {
+    int funcRet = -1;
     vector<vector<int>> cGrid(N, vector<int>(N, WALL));
     vector<P> roots;
     
@@ -47,50 +115,29 @@ void doWithComb(vector<int>& cv)
     for (const int& i : cv)
         roots.push_back(viruses[i]);
         
-    // root 좌표들을 0으로
+    // root 좌표들 그리드에 입력
     for (const P& p : roots)
-        cGrid[p.first][p.second] = 0;
+        cGrid[p.first][p.second] = VIRUS;
     
-    // 일단 첫 step 돌림.
+    // bfs
     spread(cGrid, roots);
     
-    
-    /*** DEBUG ***/
-    for (int i=0;i<N;++i)
-    {
-        for (int j=0;j<N;++j)
-        {
-            if (cGrid[i][j] == WALL) cout << '#';
-            else if (cGrid[i][j] == BLANK) cout << '.';
-            else cout << cGrid[i][j];
-        }
-        cout << endl;
-    }
-    cout << "================" << endl;
-    
-    
-    
     // 클리어 가능한 격자인지 판정
-    if (!isAllFilled(cGrid)) return;
-    
-    while (!roots.empty())
-    {
-        // 이 과정에서 roots가 하나씩 지워지고 cGrid는 변형됨, 또 겹치는 좌표는 roots에 추가됨.
-        spread(cGrid, roots);
-    }
+    if (!isAllFilled(cGrid)) return -1;
     
     // 답안 구하기 o(25000)
     for (int i=0;i<N;++i)
     {
         for (int j=0;j<N;++j)
         {
-            for (const P& v : viruses)
-                if (v.first == i && v.second == j) continue;
-            
-            ret = max(ret, cGrid[i][j]);
+            bool skip = false;
+            for (int vv=0;vv<virusCnt;++vv)
+                if (viruses[vv].first == i && viruses[vv].second == j) skip = true;
+
+            if (!skip) funcRet = max(funcRet, cGrid[i][j]);
         }
     }
-    return;
+    return funcRet;
 }
 
 void combination(int pick, int searched, vector<int>& cv)
@@ -104,11 +151,9 @@ void combination(int pick, int searched, vector<int>& cv)
     // -> pick이 7이고 searched가 5인데 cv.size()가 0이면 어차피 남은 어떤 경우에도 7개를 뽑을 수 가 없으므로 곧장 탐색 종료시켜버려도 됨.)
     if (cv.size() == pick)
     {
-        /*** DEBUG ***/
-        for (auto i : cv) cout << i << ", ";
-        cout << endl;
-        
-        return doWithComb(cv);
+        int tmp = doWithComb(cv);
+        if (tmp != -1) ret = min(ret, tmp);
+        return;
     }
     else if ((9-searched) < (pick-cv.size())) return; // TODO: +-1 오차있을수도, 검증 필요
     else if (virusCnt-1 <= searched) return; // e.g. 바이러스가 3갠데 viruses[3 이상]을 탐색하면 안되기 때문
@@ -167,6 +212,9 @@ int main()
         }
     }
     
+    bool hasVirus = false;
+    bool hasBlank = false;
+    
     cin >> N >> M;
     for (int i=0;i<N;++i)
     {
@@ -178,14 +226,26 @@ int main()
                 viruses[virusCnt].first = i;
                 viruses[virusCnt].second = j;
                 virusCnt++;
+                hasVirus = true;
             }
+            else if (initGrid[i][j] == 0) hasBlank = true;
         }
+    }
+    
+    // 예외 케이스: 이미 모든 빈칸이 바이러스로 찼을 경우(바이러스가 최소 하나는 존재해야함)
+    if (!hasBlank && hasVirus)
+    {
+        cout << 0 << '\n';
+        return 0;
     }
     
     // 10 C M
     // 최대 M개 활성화 시킬수 있을 때 최대한 많이(=M개만큼) 활성화시키는게 무조건 유리함
     vector<int> v;
     combination(M, -1, v);
+    
+    if (ret == INF) ret = -1;
+    cout << ret << '\n';
 
     return 0;
 }
